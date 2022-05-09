a924de7b47fa0b44b9685993b3815580afdfa32b
SheepSeb <naesebi2000@gmail.com>
2022-05-09 17:11:53 +0300

    Added Login component

---
+++ searchy/src/routes/loading.svelte
@@ -0,0 +1,40 @@
+<link
+  rel="stylesheet"
+  href="https://fonts.googleapis.com/icon?family=Material+Icons"
+/>
+<!-- Roboto -->
+<link
+  rel="stylesheet"
+  href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,600,700"
+/>
+<!-- Roboto Mono -->
+<link
+  rel="stylesheet"
+  href="https://fonts.googleapis.com/css?family=Roboto+Mono"
+/>
+
+<link rel="stylesheet" href="node_modules/svelte-material-ui/bare.css" />
+<link
+  rel="stylesheet"
+  href="https://cdn.jsdelivr.net/npm/svelte-material-ui@6.0.0/bare.min.css"
+/>
+<style>
+    .center{
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    margin-top: 20rem;
+    margin-bottom: 20rem;
+    width: 50%;
+    }
+  </style>
+
+<div style="display: flex; justify-content: center" class="center">
+    <CircularProgress style="height: 150px; width: 150px;" indeterminate />
+</div>
+  
+<script lang="ts">
+    import CircularProgress from '@smui/circular-progress';
+</script>
+  
+
--- searchy/src/routes/login.svelte
+++ searchy/src/routes/login.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
-    import Button, { Label } from '@smui/button';
-    import Tab from '@smui/tab';
-    import TabBar from '@smui/tab-bar';
-    import TopAppBar, { Row, Section, Title } from '@smui/top-app-bar';
-        
+    import Button, { Label } from '@smui/button';  
+    import Textfield from '@smui/textfield';
+    import HelperText from '@smui/textfield/helper-text';
+
+    let value_email = '';
+    // MARE SECURITY BUG AICISEA
+    let value_pswd = '';
 </script>
 
 <link
@@ -27,9 +29,34 @@
   href="https://cdn.jsdelivr.net/npm/svelte-material-ui@6.0.0/bare.min.css"
 />
 
-<TopAppBar>
-</TopAppBar>
+<style>
+  .center{
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 4rem;
+  margin-bottom: 4rem;
+  width: 50%;
+  }
+</style>
 
-<Button>
-    <Label>Done</Label>
-</Button>
+
+
+<div class="center">
+  <img src="https://picsum.photos/200" class="center" />
+  <Textfield variant="outlined" bind:value={value_email} label="Email" type="email">
+    <HelperText slot="helper">user@psw.com</HelperText>
+  </Textfield>
+  <br>
+  <br>
+  <div>
+    <Textfield variant="outlined" bind:value={value_pswd} label="Password" type="password">
+      <HelperText slot="helper">Choose a strong password</HelperText>
+    </Textfield>
+  </div>
+  <br>
+  <br>
+  <Button>
+    <Label>Login</Label>
+  </Button>
+</div>
